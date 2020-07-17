0ec7aa56471ef3d6674595b6d1bf80de31efa935
USER <sojib.19991018@gmail.com>
2020-07-17 22:52:36 +0600

    First time execute for trail

--- springjdbc/src/main/java/com/spring/jdbc/App.java
+++ springjdbc/src/main/java/com/spring/jdbc/App.java
@@ -1,5 +1,9 @@
 package com.spring.jdbc;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.jdbc.core.JdbcTemplate;
+
 /**
  * Hello world!
  *
@@ -8,6 +12,16 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "MY program started........." );
+        //Spring jdbc=> jdbcTemplate
+        ApplicationContext context=new
+        		ClassPathXmlApplicationContext("com/spring/jdbc/config.xml");
+        JdbcTemplate template = context.getBean("jdbcTemplate",JdbcTemplate.class);
+        //insert query
+        String query="insert into student(id,name,city) values(?,?,?)";
+        //fire query
+        int result = template.update(query,11294,"Sojib","Mymensingh");
+        System.out.println("Number of record inserted..."+result);
+        
     }
 }
